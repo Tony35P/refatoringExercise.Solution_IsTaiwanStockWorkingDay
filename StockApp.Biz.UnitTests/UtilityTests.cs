b02df746bb916c698dcb810ed2679de73265dfa5
Tony_Tsai <tonytsai27@gmail.com>
2022-01-01 18:18:53 +0800

    之所以這樣寫是因為原本IsTaiwanStockWorkingDay()是private不想讓外界叫用, 但改成了protected讓子類別可呼叫他，這樣就不用改成public

--- StockApp.Biz.UnitTests/UtilityTests.cs
+++ StockApp.Biz.UnitTests/UtilityTests.cs
@@ -1,9 +1,14 @@
 using NUnit.Framework;
+using System;
 
 namespace StockApp.Biz.UnitTests
 {
     public class UtilityTests
     {
-        public class TestableUtility : Utility { }
+        public class TestableUtility : Utility 
+        {
+            public bool IsTaiwanStockWorkingDay(DateTime today) => base.IsTaiwanStockWorkingDay(today);
+        }
+        
     }
 }
