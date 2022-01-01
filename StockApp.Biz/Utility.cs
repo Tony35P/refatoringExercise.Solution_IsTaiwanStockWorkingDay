4e2c619051cbf689d53e394fd823e33fbc43175a
Tony_Tsai <tonytsai27@gmail.com>
2022-01-01 19:10:44 +0800

    開始重構, 改掉magic number

--- StockApp.Biz/Utility.cs
+++ StockApp.Biz/Utility.cs
@@ -10,10 +10,12 @@ namespace StockApp.Biz
     {
         public bool IsTaiwanStockWorkingDay(DateTime today)
         {
+            const int beginWorkingDay = 1, endWorkingDay = 5;
+            
             DateTime start = new DateTime(today.Year, today.Month, today.Day, 9, 0, 0, 0);
             DateTime end = new DateTime(today.Year, today.Month, today.Day, 13, 30, 0, 0);
             int weekday = int.Parse(today.DayOfWeek.ToString("d"));
-            if ((weekday >= 1 && weekday <= 5) && (today.CompareTo(start) >= 0 && today.CompareTo(end) <= 0))
+            if ((weekday >= beginWorkingDay && weekday <= endWorkingDay) && (today.CompareTo(start) >= 0 && today.CompareTo(end) <= 0))
                 return true;
             return false;
 
