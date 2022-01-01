a19a3051885991dec5b3cd20148d04e3842e8730
Tony_Tsai <tonytsai27@gmail.com>
2022-01-01 19:16:46 +0800

    重構 起訖時間

--- StockApp.Biz/Utility.cs
+++ StockApp.Biz/Utility.cs
@@ -11,11 +11,17 @@ namespace StockApp.Biz
         public bool IsTaiwanStockWorkingDay(DateTime today)
         {
             const int beginWorkingDay = 1, endWorkingDay = 5;
-            
-            DateTime start = new DateTime(today.Year, today.Month, today.Day, 9, 0, 0, 0);
-            DateTime end = new DateTime(today.Year, today.Month, today.Day, 13, 30, 0, 0);
+            TimeSpan beginWorkingHour = TimeSpan.FromHours(9);
+            TimeSpan endWorkingHour = TimeSpan.FromHours(13.5);
+
+            //DateTime start = new DateTime(today.Year, today.Month, today.Day, 9, 0, 0, 0);
+            //DateTime end = new DateTime(today.Year, today.Month, today.Day, 13, 30, 0, 0);
+            DateTime beginTime = today.Date.Add(beginWorkingHour);
+            DateTime endTime = today.Date.Add(endWorkingHour);
+
+
             int weekday = int.Parse(today.DayOfWeek.ToString("d"));
-            if ((weekday >= beginWorkingDay && weekday <= endWorkingDay) && (today.CompareTo(start) >= 0 && today.CompareTo(end) <= 0))
+            if ((weekday >= beginWorkingDay && weekday <= endWorkingDay) && (today.CompareTo(beginTime) >= 0 && today.CompareTo(endTime) <= 0))
                 return true;
             return false;
 
