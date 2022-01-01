e7c590de5147e2a9b658da761500cfb85fe1235c
Tony_Tsai <tonytsai27@gmail.com>
2022-01-01 19:00:34 +0800

    把單元測試寫好，避免等等改壞。且藉由單元測試便可發現這隻副程式有錯

--- StockApp.Biz.UnitTests/UtilityTests.cs
+++ StockApp.Biz.UnitTests/UtilityTests.cs
@@ -5,9 +5,42 @@ namespace StockApp.Biz.UnitTests
 {
     public class UtilityTests
     {
+        
+        [TestCase("2022/1/1")]
+        [TestCase("2022/1/2")]
+        public void IsTaiwanStockWorkingDay_星期六日傳回false(string dt)
+        {
+            DateTime today = Convert.ToDateTime(dt);
+
+            bool actual = new TestableUtility().TestIsTaiwanStockWorkingDay(today);
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestCase("2022/1/3 8:59:59")]
+        [TestCase("2022/1/3 13:30:01")]
+        public void IsTaiwanStockWorkingDay_平日非營業時間傳回false(string dt)
+        {
+            DateTime today = Convert.ToDateTime(dt);
+
+            bool actual = new TestableUtility().TestIsTaiwanStockWorkingDay(today);
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestCase("2022/1/3 9:00:01")]
+        [TestCase("2022/1/3 13:29:59")]
+        public void IsTaiwanStockWorkingDay_星期一至五營業時間傳回true(string dt)
+        {
+            DateTime today = Convert.ToDateTime(dt);
+
+            bool actual = new TestableUtility().TestIsTaiwanStockWorkingDay(today);
+
+            Assert.IsTrue(actual);
+        }
         public class TestableUtility : Utility 
         {
-            public bool IsTaiwanStockWorkingDay(DateTime today) => base.IsTaiwanStockWorkingDay(today);
+            public bool TestIsTaiwanStockWorkingDay(DateTime today) => base.IsTaiwanStockWorkingDay(today);
         }
         
     }
