43889014e0db4f4aa44899f1a8c63a156328217b
Tony_Tsai <tonytsai27@gmail.com>
2022-01-01 19:04:06 +0800

    這裡不應該帶入DateTime.Now, 因為會讓這隻程式再不同天得到不同結果, 不符合純函數的概念, 務必要多注意

--- StockApp.Biz/Utility.cs
+++ StockApp.Biz/Utility.cs
@@ -12,7 +12,7 @@ namespace StockApp.Biz
         {
             DateTime start = new DateTime(today.Year, today.Month, today.Day, 9, 0, 0, 0);
             DateTime end = new DateTime(today.Year, today.Month, today.Day, 13, 30, 0, 0);
-            int weekday = int.Parse(DateTime.Now.DayOfWeek.ToString("d"));
+            int weekday = int.Parse(today.DayOfWeek.ToString("d"));
             if ((weekday > 1 && weekday < 5) && (today.CompareTo(start) >= 0 && today.CompareTo(end) <= 0))
                 return true;
             return false;
